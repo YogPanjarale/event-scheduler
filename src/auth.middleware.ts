0ee535906acee0fe50e283937eda4cbb75b1590c
Yog <yog.panjarale@gmail.com>
2021-12-13 20:59:01 +0530

    PROJECT reset

--- src/auth.middleware.ts
+++
@@ -1,9 +0,0 @@
-import {Request,Response,NextFunction}from 'express';
-const authMiddleware = (req:Request,res:Response,next:NextFunction)=>{
-    if (req.headers.authorization!==process.env.AUTH_TOKEN) {
-        return res.status(401).send('Unauthorized');
-    }
-
-    return next()
-}
-export default authMiddleware;
