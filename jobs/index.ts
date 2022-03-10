6ef25d468c215b55077aa8fbc7426a6fd5229807
Yog <yog.panjarale@gmail.com>
2022-03-10 18:48:00 +0530

    reset

--- jobs/index.ts
+++
@@ -1,8 +0,0 @@
-import {Obj,IJob} from '../types';
-const logJob = (params:Obj):void => {
-    const message = params['message'];
-    console.log(message);
-};
-const jobs = new Map<string,IJob>()
-jobs.set('log', logJob);
-export default jobs;
